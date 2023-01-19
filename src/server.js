c77d58c1f2c58cc18dfb22bf1ae2444294afcf44
unknown <ankits.8080@gmail.com>
2023-01-19 18:57:35 +0530

    Added

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express=require("express");
 const cors=require("cors");
 const app=express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 const connect=require("./config/db");
 
