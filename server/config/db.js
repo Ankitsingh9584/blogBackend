a86d4ddbbc73baab6ea9390e10298586e0b11525
unknown <ankits.8080@gmail.com>
2023-01-19 20:30:59 +0530

    Sdbsbf

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,10 @@
 const mongoose=require("mongoose");
 // mongodb://127.0.0.1:27017/blog
  const connect=async()=>{
-return mongoose.connect(process.env.MONGODB_URL)
+return mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
 }
 
 module.exports=connect;
